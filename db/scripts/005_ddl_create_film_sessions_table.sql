c266c44a9c538a7d0d49cb132fe5575fc3a24730
OvercomingJunk <overcomingjunk@yandex.ru>
2023-02-09 18:03:26 +0300

    Created cinema database, connected Liquibase and SQL scripts executed.

---
+++ db/scripts/005_ddl_create_film_sessions_table.sql
@@ -0,0 +1,8 @@
+create table if not exists film_sessions
+(
+    id         serial primary key,
+    film_id    int references films (id),
+    halls_id   int references halls (id),
+    start_time timestamp,
+    end_time   timestamp
+);
---
+++ db/scripts/008_dml_insert_files.sql
@@ -0,0 +1,11 @@
+insert into files(name, path) values
+                                  ('avatar_the_way_of_water', 'files/avatar_the_way_of_water.jpeg'),
+                                  ('black_adam', 'files/black_adam.jpeg'),
+                                  ('black_panther_wakanda_forever', 'files/black_panther_wakanda_forever.jpeg'),
+                                  ('knives_out_glass_onion', 'files/knives_out_glass_onion.jpeg'),
+                                  ('luck', 'files/luck.jpeg'),
+                                  ('the_batman', 'files/the_batman.jpeg'),
+                                  ('the_menu', 'files/the_menu.jpeg'),
+                                  ('top_gun_maverick', 'files/top_gun_maverick.jpeg'),
+                                  ('turning_red', 'files/turning_red.jpeg'),
+                                  ('winnie_the_pooh_blood_and_honey', 'files/winnie_the_pooh_blood_and_honey.jpeg');
