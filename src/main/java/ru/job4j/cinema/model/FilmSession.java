509d6822e9e9f0775d1e22b02331cd5bd259a6a1
OvercomingJunk <overcomingjunk@yandex.ru>
2023-02-09 23:01:50 +0300

    Created data models

---
+++ src/main/java/ru/job4j/cinema/model/FilmSession.java
@@ -0,0 +1,95 @@
+package ru.job4j.cinema.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 09.02.2023
+ */
+public class FilmSession {
+
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+      "id", "id",
+      "film_id", "filmId",
+      "halls_id", "hallsId",
+      "start_time", "startTime",
+      "end_time", "endTime"
+    );
+
+    private int id;
+
+    private int filmId;
+
+    private int hallsId;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    public FilmSession(int filmId, int hallsId, LocalDateTime startTime, LocalDateTime endTime) {
+        this.filmId = filmId;
+        this.hallsId = hallsId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public int getHallsId() {
+        return hallsId;
+    }
+
+    public void setHallsId(int hallsId) {
+        this.hallsId = hallsId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FilmSession that = (FilmSession) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/cinema/model/Genre.java
@@ -0,0 +1,52 @@
+package ru.job4j.cinema.model;
+
+import java.util.Objects;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 09.02.2023
+ */
+public class Genre {
+
+    private int id;
+
+    private String name;
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || o.getClass() != getClass()) {
+            return false;
+        }
+        Genre genre = (Genre) o;
+        return id == genre.id && Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
