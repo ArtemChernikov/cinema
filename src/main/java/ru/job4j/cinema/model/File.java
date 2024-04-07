7df9f7a8fa7defe040f3f0a69b69e96cd8d37241
Artem Chernikov <overcomingjunk@yandex.ru>
2024-04-07 21:56:05 +0300

    delete: удалить старые тесты

--- src/main/java/ru/job4j/cinema/model/File.java
+++ src/main/java/ru/job4j/cinema/model/File.java
@@ -1,6 +1,7 @@
 package ru.job4j.cinema.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
 @Entity
 @Table(name = "files")
 public class File {
