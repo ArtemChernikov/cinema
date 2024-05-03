ab1511aed8304bd2a7523637c024c96cde710a44
Artem Chernikov <overcomingjunk@yandex.ru>
2024-05-03 12:25:46 +0300

    feat: Добавить модуль для подключения к Kinopoisk API

---
+++ kinopoisk-api/src/main/java/ru/cinema/controller/KinopoiskController.java
@@ -0,0 +1,31 @@
+package ru.cinema.controller;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.cinema.KinopoiskClient;
+import ru.cinema.model.response.Response;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 28.04.2024
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/kinopoisk")
+public class KinopoiskController {
+    private final KinopoiskClient kinopoiskClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PatchMapping("/update")
+    public void updateFilms() {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Response response = objectMapper.convertValue(kinopoiskClient.getTop10Films().getBody(), Response.class);
+        System.out.println("Total documents: " + response.getTotal());
+        System.out.println(response);
+    }
+}
