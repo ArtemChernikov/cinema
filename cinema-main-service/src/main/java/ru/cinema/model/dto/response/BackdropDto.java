1b6c0d674604ead21898c0aefd42ef885ae9c622
Artem Chernikov <overcomingjunk@yandex.ru>
2024-05-08 18:06:39 +0300

    feat: добавить dto для получения всех фильмов из микросервиса films

---
+++ cinema-main-service/src/main/java/ru/cinema/model/dto/response/BackdropDto.java
@@ -0,0 +1,18 @@
+package ru.cinema.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 07.05.2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BackdropDto {
+    private String url;
+    private String previewUrl;
+}
---
+++ cinema-main-service/src/main/java/ru/cinema/model/dto/response/FilmDto.java
@@ -0,0 +1,32 @@
+package ru.cinema.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 07.05.2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilmDto {
+    private Long id;
+    private String name;
+    private String alternativeName;
+    private String type;
+    private Integer year;
+    private String description;
+    private String shortDescription;
+    private RatingDto rating;
+    private Integer movieLength;
+    private Integer ageRating;
+    private PosterDto poster;
+    private BackdropDto backdrop;
+    private Set<String> genres;
+    private Set<String> countries;
+}
