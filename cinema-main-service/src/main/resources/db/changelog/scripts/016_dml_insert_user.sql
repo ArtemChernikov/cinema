34f01803fcbb87a6eca88a76e174b54dd599f109
Artem Chernikov <overcomingjunk@yandex.ru>
2024-05-17 17:20:47 +0300

    feat: добавить администратора

---
+++ cinema-main-service/src/main/resources/db/changelog/scripts/016_dml_insert_user.sql
@@ -0,0 +1,2 @@
+insert into users (name, surname, email, username, password, role_id)
+VALUES ('Vladimir', 'Ivanov', 'admin@gmail.com', 'admin', '$2a$12$IuuRj3oNgcrZDKz2Dxjak.brTyD5HDuuiyeZZXwM6hECQpjH7j.I.', 1);
